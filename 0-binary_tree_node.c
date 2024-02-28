636bfc34631b4765e8127cd66a9f3a105fd75c1e
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-02-28 15:59:08 -0800

    :zap:

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,4 @@
 #include "binary_trees.h"
-
-
 /**
  * binary_tree_node - creates a binary tree node
  * @parent: pointer to the parent node of the node to create
@@ -13,7 +11,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t * new;
-
+    
     new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
     if (!new)
         return (NULL);
