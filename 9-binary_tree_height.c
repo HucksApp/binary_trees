5b89a3a68eeabb6c6250f6f0ed512a1cdad4c622
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-02-28 20:28:05 -0800

    :zap:

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,10 +13,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-  
-  if (tree->left)
-	  l_height = 1 + binary_tree_height(tree->left);
-  if (tree->right)
-	  r_height = 1 + binary_tree_height(tree->right);
-	 return ((l_height > r_height) ? l_height : r_height);
+  	if (tree->left)
+		l_height = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		r_height = 1 + binary_tree_height(tree->right);
+	return ((l_height > r_height) ? l_height : r_height);
 }
