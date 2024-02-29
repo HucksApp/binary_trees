f23a17bfddca449ab8eccb50f998cebe2d84adb0
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-02-28 16:08:38 -0800

    :zap:

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,17 +7,16 @@
  * Return: Pointer to the newly created node
  *         NULL on failure
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
+	binary_tree_t *new;
 
-    new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-    if(!new)
-        return (NULL);
-    new->parent = parent;
+	new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+	new->parent = parent;
     new->n = value;
-    new->left = NULL;
-    new->right = NULL;
-    return (new);
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
 }
