093ed23f7d2afba34975cb373c28db82ff34afc0
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-02-28 16:03:57 -0800

    :zap:

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,9 +11,9 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new;
-    
+
     new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-    if (!new)
+    if(!new)
         return (NULL);
     new->parent = parent;
     new->n = value;
