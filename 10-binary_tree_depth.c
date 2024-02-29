e62769eb38d2829be29203cea3290d270b0d58f7
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-02-29 04:02:30 -0800

    :zap:

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,16 +9,12 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth = 0;
+	size_t depth;
 
 	if (!tree)
 		return (0);
 
-	while (tree->parent)
-	{
-		depth++;
-		tree = tree->parent;
-	}
-
+	for (depth = 0; tree->parent; tree = tree->parent, depth++)
+		;
 	return (depth);
 }
